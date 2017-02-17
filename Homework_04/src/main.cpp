f92fcce6387ffea5fcbc37b28794cc31fed0cb92
MariosTech <contact@marios.tech>
2017-02-16 18:40:00 -0800

    Added SLinkedList from GitHub Page

--- Homework_04/src/main.cpp
+++ Homework_04/src/main.cpp
@@ -2,6 +2,14 @@
 // Created by Mario Personal on 2/16/17.
 //
 
+#include <iostream>
+
+#include "../include/SLinkedList.cpp"
+
+using namespace std;
+
 int main() {
+    SLinkedList<string> words;
+
     return 0;
 }
