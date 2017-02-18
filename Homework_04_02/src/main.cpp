e4c92c600475e4ad67c031596fabac2620bbe477
MariosTech <contact@marios.tech>
2017-02-17 20:01:15 -0800

    Added Base Code for Homework 4

--- Homework_04_02/src/main.cpp
+++ Homework_04_02/src/main.cpp
@@ -1,7 +1,44 @@
-//
-// Created by Mario Personal on 2/17/17.
-//
+#include <stdexcept>
+#include <iostream>
+#include <string>
+#include "../include/SNodeList.h"
+
+
+using namespace std;
+
+class Student {
+private:
+    string _name;
+    int _nsemesters;
+
+public:
+    void print() {
+        cout << "Name:" << _name << "; No. of semesters=" << _nsemesters << endl;
+    }
+    void setName(string name) {
+        _name = name;
+    }
+    void setNSemesters(int n) {
+        _nsemesters = n;
+    }
+    void updateNSemesters() {
+        _nsemesters++;
+    }
+};
 
 int main() {
-    return 0;
+
+    SNodeList<Student> students;
+
+    Student s;
+    for (int i = 0; i < 5; i++) {
+        s.setName("Student_" + to_string(i));
+        s.setNSemesters(2);
+        students.addFront(s);
+    }
+
+    for (SNodeList<Student>::Iterator i = students.begin(); i != students.end(); ++i) {
+        i.get().print();
+    }
+    // system("pause");
 }
