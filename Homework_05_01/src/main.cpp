3295b50a2966547eea3ceda1a0cae7012cece686
MariosTech <contact@marios.tech>
2017-03-02 12:39:23 -0800

    Updated Project to Add Homework 05

---
+++ Homework_05_01/src/main.cpp
@@ -0,0 +1,16 @@
+//
+// Created by Mario Personal on 3/2/17.
+//
+
+#include <iostream>
+#include <string>
+#include "../include/DLinkedList.h"
+
+using namespace std;
+
+int main()
+{
+    DLinkedList<string> lyrics;
+    // YOUR CODE TO CREATE THE LIST GOES HERE
+    cout << lyrics.front() << endl;
+}
